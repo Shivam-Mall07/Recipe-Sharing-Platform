2c20f4a6dbfd7a2cf3100ae50fd8dad5f667287c
Shivam Mall <shivammall035@gmail.com>
2026-07-26 14:35:37 +0530

    Fix Vercel deployment

--- server/server.js
+++ server/server.js
@@ -61,6 +61,11 @@ app.use((err, req, res, next) => {
 
 // Configure server listening port
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+module.exports = app;
